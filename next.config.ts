c291372fd1a7c4023b72b206fb51fb6ede11068a
Aleksey <47826423+Aleksey1103@users.noreply.github.com>
2025-09-13 09:03:02 +0000

    feat: update next config; add allowedOrigins;

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: { remotePatterns: [new URL('https://lh3.googleusercontent.com/**')] },
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        process.env['AUTH_URL'], // Add your GitHub Codespace URL
+        "localhost:3000", // Allow local development
+      ],
+    },
+  },
 };
 
 export default nextConfig;

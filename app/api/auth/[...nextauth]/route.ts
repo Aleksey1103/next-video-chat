5608f0c4820a12a6fa0efd4d54c5b02b5f2d55b8
Aleksey <47826423+Aleksey1103@users.noreply.github.com>
2025-09-13 14:12:01 +0000

    fix: fix quotes type

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,4 @@
 import { handlers } from '@/lib/auth';
 // Referring to the auth.ts we just created
-export const dynamic = "force-static"
+export const dynamic = 'force-static';
 export const { GET, POST } = handlers;

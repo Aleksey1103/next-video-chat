38eef89b79da570201a4f0e5cc890cf4c8f638c0
alekseyl <a.limarev@fim.ltd>
2025-09-13 17:55:16 +0300

    feat: update auth route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,6 @@
 import { handlers } from '@/lib/auth';
 // Referring to the auth.ts we just created
-export const dynamic = 'force-static';
-export const { GET, POST } = handlers;
+const dynamic = 'force-dynamic';
+const { GET, POST } = handlers;
+
+export { dynamic, GET, POST };
